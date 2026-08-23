ea536adf771939371555f495cd7033e9f77866f2
Amogh-007-Rin <amoghdath233@gmail.com>
2026-08-23 20:41:36 +0100

    Removed-old-junk

--- server/src/tests/test.db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-
-export async function verifyData(){
-    const data = await mongoose.connection.db?.collection('user').countDocuments();
-    console.log(`current data count ${data}`);
-    return data;
-}
